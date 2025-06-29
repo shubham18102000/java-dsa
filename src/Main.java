9b1d55a5944bc9a3f04b8d880b1f375843422b18
shubham18102000 <shubhamchamoli8218035988@gmail.com>
2025-06-29 16:02:22 +0530

    shubham chamli

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        LargestArray a = new LargestArray();
-        int result = a.largestwas();
-        System.out.println("The largest number is: " + result);
+//        LargestArray a = new LargestArray();
+//        int result = a.largestwas();
+//        System.out.println("The largest number is: " + result);
 
         Binarysearch b = new Binarysearch();
         int k = b.binaryseach(5);
